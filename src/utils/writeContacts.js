345945b8bc0056ae134e9eaa5b1fd7b692451790
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-09-29 09:10:39 +0300

    Wrote logic for scripts

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,5 @@
+import * as fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (contacts) =>
+  fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
